67fc48bf62d8bcbe1fccd39255a5a0187cb91236
Moses Madavaram <mosesgab@gmail.com>
2024-08-31 23:02:58 -0400

    Set up some parts of the database and made corrections on the methods when listing "app.route" in routes.py.

--- routes.py
+++ routes.py
@@ -15,7 +15,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db.init_app(app)
 
 
-@app.route("/", requests=["POST", "GET"])
+@app.route("/", methods=["POST", "GET"])
 def index():
     if request.method == "POST":
         preferred_genre = request.form.get("genre")
@@ -24,26 +24,28 @@ def index():
                                episodes=preferred_episodes)
     return render_template("index.html")
 
-@app.route("/logout", requests=["POST","GET"])
-def logout():
 
-
-
-
-
-@app.route("/templates/recommendation.html", requests=["POST", "GET"])
+@app.route("/templates/recommendation.html", methods=["POST", "GET"])
 def recommendation():
     #  query = flask.request.args
     return render_template("recommendation.html")
 
 
-@app.route("/submission.html", request=["POST", "GET"])
+@app.route("/submission.html", methods=["POST", "GET"])
 def submission():
     if request.method == "POST":
         genre_recommendation = request.form.get("genre")
         episode_length = request.form.get("episode")
         recommendations = []
         return render_template("submission.html")
+
+
+@app.route("/templates/register.html", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        name = request.form.get("name")
+        email = request.form.get("email")
+        password = request.form.get("password")
 
 
 if __name__ == '__main__':
