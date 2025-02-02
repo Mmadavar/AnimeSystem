6615a86d3730f8725123e57a0267af07c578a085
Moses Madavaram <mosesgab@gmail.com>
2025-02-01 20:59:25 -0500

    Enhanced the recommendation algorithm, did some routing work, and did some more styling on the web pages by generating code using Bootstrap.

--- Recommendation_Algorithm.py
+++ Recommendation_Algorithm.py
@@ -3,36 +3,31 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 #load the data and preprocess it
-data = pd.read_csv('anime.csv', usecols=["genre", "names", "episodes"])
-
-
+data = pd.read_csv('anime.csv', usecols=["genre", "name", "episodes"])
 
 #convert episodes into numerical vectors using TF-IDF TfidfVectorizer.
-data["episodes"] = pd.to_numeric(data["episodes"]).astype(int)
+data["episodes"] = pd.to_numeric(data["episodes"], errors='coerce').fillna(0).astype(int)
+data["genre"] = data["genre"].fillna("").str.lower()
 
-df = TfidfVectorizer(stop_words='english')
-df_matrix = df.fit_transform(data["episodes"])
-
-
-#compute cosine similarily between TF-IDF vectors
-cosine_sim = cosine_similarity(df_matrix)
+tfidf = TfidfVectorizer(stop_words='english')
+tfidf_matrix = tfidf.fit_transform(data["genre"])
 
 
 # function to get recommendations based on cosine similarity
-def recommendation(episodes, genre, recommendations=5):
+def recommendation(episodes, genre, total):
 
-    # Create a vector for the input genre
-    input_vector = df.transform([genre])
+    # tranform input genre into a vector
+    input_vector = tfidf.transform([genre.lower()])
 
     # Compute similarity between the input and all anime genres
-    genre_similarities = cosine_similarity(input_vector, df_matrix).flatten()
+    genre_similarities = cosine_similarity(input_vector, tfidf_matrix).flatten()
 
     # Filter based on episode length and sort by similarity
-    df["similarity"] = genre_similarities
-    filtered_df = df[df["episodes"] <= episodes]
-    recommendations = filtered_df.sort_values(by="similarity", ascending=False).head(recommendations)
+    data["similarity"] = genre_similarities
+    filtered_df = data[data["episodes"] <= episodes]
+    recommendations = filtered_df.sort_values(by="similarity", ascending=False)
 
-    return recommendations["name"].tolist()
+    return recommendations.head(total)[["name", "genre", "episodes"]]
 
 
 
